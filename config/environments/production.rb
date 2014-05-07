7bfc21e9a0ad4e3959c46e164444325a26919433
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2014-05-07 21:59:33 +0200

    rails4: adding css compressor in production.
    reference: https://github.com/alindeman/upgradingtorails4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,7 @@ Wingolfsplattform::Application.configure do
 
   # Compress JavaScripts and CSS
   config.assets.js_compressor = :uglifier
+  config.assets.css_compressor = :sass
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
