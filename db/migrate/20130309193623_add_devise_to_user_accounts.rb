01f7d3182387aaca99564216661bb7b222fee084
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2014-05-07 21:22:33 +0200

    rails4: fixing db migrations.
    **Fix Migrations**:
    
    * Do not use migrations for adding flags. Instead, repair tasks should be used, I think.
    * In `AddDeviseToUserAccounts`, do not create duplicate columns, but change the existing ones of the `user_accounts` table. [See this SO answer](http://stackoverflow.com/a/12990129/2066546).
    
    Trello: https://trello.com/c/Dlio0wG9/281-rails-4

--- db/migrate/20130309193623_add_devise_to_user_accounts.rb
+++ db/migrate/20130309193623_add_devise_to_user_accounts.rb
@@ -1,9 +1,10 @@
 class AddDeviseToUserAccounts < ActiveRecord::Migration
   def self.up
     change_table(:user_accounts) do |t|
+      
       ## Database authenticatable
       #t.string :email,              :null => false, :default => ""
-      t.string :encrypted_password, :null => false, :default => ""
+      t.change :encrypted_password, :string, :null => false, :default => ""
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -9,68 +9,68 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131115130631) do
+ActiveRecord::Schema.define(version: 20131115130631) do
 
-  create_table "attachments", :force => true do |t|
+  create_table "attachments", force: true do |t|
     t.string   "file"
     t.string   "title"
     t.text     "description"
     t.integer  "parent_id"
     t.string   "parent_type"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.string   "content_type"
     t.integer  "file_size"
   end
 
-  create_table "bookmarks", :force => true do |t|
+  create_table "bookmarks", force: true do |t|
     t.integer  "bookmarkable_id"
     t.string   "bookmarkable_type"
     t.integer  "user_id"
-    t.datetime "created_at",        :null => false
-    t.datetime "updated_at",        :null => false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
   end
 
-  create_table "bv_mappings", :force => true do |t|
+  create_table "bv_mappings", force: true do |t|
     t.string   "bv_name"
     t.string   "plz"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "dag_links", :force => true do |t|
+  create_table "dag_links", force: true do |t|
     t.integer  "ancestor_id"
     t.string   "ancestor_type"
     t.integer  "descendant_id"
     t.string   "descendant_type"
     t.boolean  "direct"
     t.integer  "count"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.datetime "valid_to"
     t.datetime "valid_from"
   end
 
-  create_table "events", :force => true do |t|
+  create_table "events", force: true do |t|
     t.string   "name"
     t.text     "description"
     t.datetime "start_at"
     t.datetime "end_at"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
-  create_table "flags", :force => true do |t|
+  create_table "flags", force: true do |t|
     t.string   "key"
     t.integer  "flagable_id"
     t.string   "flagable_type"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
-  create_table "geo_locations", :force => true do |t|
+  create_table "geo_locations", force: true do |t|
     t.string   "address"
     t.float    "latitude"
     t.float    "longitude"
@@ -78,23 +78,23 @@ ActiveRecord::Schema.define(:version => 20131115130631) do
     t.string   "country_code"
     t.string   "city"
     t.string   "postal_code"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.datetime "queried_at"
   end
 
-  add_index "geo_locations", ["address"], :name => "index_geo_locations_on_address"
+  add_index "geo_locations", ["address"], name: "index_geo_locations_on_address", using: :btree
 
-  create_table "groups", :force => true do |t|
+  create_table "groups", force: true do |t|
     t.string   "name"
-    t.datetime "created_at",     :null => false
-    t.datetime "updated_at",     :null => false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
     t.string   "token"
     t.string   "extensive_name"
     t.string   "internal_token"
   end
 
-  create_table "nav_nodes", :force => true do |t|
+  create_table "nav_nodes", force: true do |t|
     t.string   "url_component"
     t.string   "breadcrumb_item"
     t.string   "menu_item"
@@ -104,21 +104,21 @@ ActiveRecord::Schema.define(:version => 20131115130631) do
     t.boolean  "hidden_menu"
     t.integer  "navable_id"
     t.string   "navable_type"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
-  create_table "pages", :force => true do |t|
+  create_table "pages", force: true do |t|
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at",     :null => false
-    t.datetime "updated_at",     :null => false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
     t.string   "redirect_to"
     t.integer  "author_user_id"
     t.string   "type"
   end
 
-  create_table "posts", :force => true do |t|
+  create_table "posts", force: true do |t|
     t.string   "subject"
     t.text     "text"
     t.integer  "group_id"
@@ -126,92 +126,92 @@ ActiveRecord::Schema.define(:version => 20131115130631) do
     t.string   "external_author"
     t.datetime "sent_at"
     t.boolean  "sticky"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.text     "entire_message"
   end
 
-  create_table "profile_fields", :force => true do |t|
+  create_table "profile_fields", force: true do |t|
     t.integer  "profileable_id"
     t.string   "label"
     t.string   "type"
     t.text     "value"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
     t.string   "profileable_type"
     t.integer  "parent_id"
   end
 
-  create_table "relationships", :force => true do |t|
+  create_table "relationships", force: true do |t|
     t.string   "name"
     t.integer  "user1_id"
     t.integer  "user2_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "status_group_membership_infos", :force => true do |t|
+  create_table "status_group_membership_infos", force: true do |t|
     t.integer  "membership_id"
     t.integer  "promoted_by_workflow_id"
     t.integer  "promoted_on_event_id"
-    t.datetime "created_at",              :null => false
-    t.datetime "updated_at",              :null => false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
-  create_table "user_accounts", :force => true do |t|
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+  create_table "user_accounts", force: true do |t|
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.integer  "user_id"
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
   end
 
-  add_index "user_accounts", ["reset_password_token"], :name => "index_user_accounts_on_reset_password_token", :unique => true
+  add_index "user_accounts", ["reset_password_token"], name: "index_user_accounts_on_reset_password_token", unique: true, using: :btree
 
-  create_table "users", :force => true do |t|
+  create_table "users", force: true do |t|
     t.string   "alias"
     t.string   "first_name"
     t.string   "last_name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.boolean  "female"
   end
 
-  create_table "workflow_kit_parameters", :force => true do |t|
+  create_table "workflow_kit_parameters", force: true do |t|
     t.string   "key"
     t.string   "value"
     t.integer  "parameterable_id"
     t.string   "parameterable_type"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
   end
 
-  create_table "workflow_kit_steps", :force => true do |t|
+  create_table "workflow_kit_steps", force: true do |t|
     t.integer  "sequence_index"
     t.integer  "workflow_id"
     t.string   "brick_name"
-    t.datetime "created_at",     :null => false
-    t.datetime "updated_at",     :null => false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
-  create_table "workflow_kit_workflows", :force => true do |t|
+  create_table "workflow_kit_workflows", force: true do |t|
     t.string   "name"
     t.string   "description"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
-  create_table "workflows", :force => true do |t|
+  create_table "workflows", force: true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
