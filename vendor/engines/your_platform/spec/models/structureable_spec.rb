f13bfe5a57b6e2e52a7fb52689b708c830638e66
Joachim Back <joachim.back@web.de>
2014-10-21 21:38:45 +0200

    Specs for cache descendants performance

--- vendor/engines/your_platform/spec/models/structureable_spec.rb
+++ vendor/engines/your_platform/spec/models/structureable_spec.rb
@@ -28,7 +28,49 @@ describe Structureable do
       @node.destroy
       @parent.links_as_parent.count.should == 0
     end
+    
+    it "should have children" do
+      @parent = create( :page )
+      @parent.child_pages << @node
+      @parent.children.count.should == 1
+    end      
 
+    it "should update the children" do
+      @parent = create( :page )
+      @parent.child_pages << @node
+      @parent.children.count.should == 1
+      @brother = create( :page )
+      @parent.child_pages << @brother
+      @parent.children.count.should == 2
+    end      
+    
+    it "should have descendants" do
+      @parent = create( :page )
+      @parent.child_pages << @node
+      @parent.descendants.count.should == 1
+    end      
+
+    it "should update the descendants after a reload" do
+      @parent = create( :page )
+      @parent.child_pages << @node
+      @parent.descendants.count.should == 1
+      @brother = create( :page )
+      @parent.child_pages << @brother
+      @parent.reload
+      @parent.descendants.count.should == 2
+    end      
+
+    it "should cache the descendants" do
+      @parent = create( :page )
+      @parent.descendants.count.should == 0
+      @parent.reload
+      t1 = Time.now
+      @parent.descendants.count.should == 0
+      t2 = Time.now
+      t3 = t2-t1
+      # without caching it took 0.009 seconds. With caching it took 0.0007 seconds
+      t3.should < 0.001
+    end      
   end
   
 end
