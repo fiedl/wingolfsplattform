8332127a2f78870991de7484201cfdf694e58a29
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2015-03-06 10:26:28 +0100

    moving some default abilities to your_platform

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -110,34 +110,27 @@ module AbilityDefinitions
   
   # ===============================================================================================
   def rights_for_signed_in_users
-    can :read, :terms_of_use
-    can :accept, :terms_of_use
+    #
+    # Inherited from your_platform:
+    # - terms of use
+    # - users
+    #   - name auto completion
+    #   - read the own user profile
+    #   - read all non-hidden user profiles
+    # - manage own non-general profile fields
+    # - set validity ranges on own memberships (for corporate vita)
+    # - join events
+    # - read groups that are not former-member groups
+    # - read pages of own groups
+    # - download attachements of those pages
+    # 
+    super
     
     # For the moment, everybody can view the statistics.
     #
     can :index, :statistics
     can :read, :statistics
     can :export, :statistics
-    
-    can :read, Page do |page|
-      page.group.nil? || page.group.members.include?(user)
-    end
-    can :download, Attachment do |attachment|
-      attachment.parent.try(:group).nil? || attachment.parent.try(:group).try(:members).try(:include?, user)
-    end
-    
-    can :read, Group do |group|
-      # Regular users cannot see the former_members_parent groups
-      # and their descendant groups.
-      #
-      not (group.has_flag?(:former_members_parent) || group.ancestor_groups.find_all_by_flag(:former_members_parent).count > 0)
-    end    
-    
-    # Regular users can read users that are not hidden.
-    # And they can read themselves.
-    #
-    can :read, User, id: User.find_all_non_hidden.map(&:id)
-    can :read, user
     
     if not read_only_mode?
       # Regular users can update their own profile.
@@ -168,40 +161,18 @@ module AbilityDefinitions
         parent_field.profileable.kind_of?(User) && (parent_field.profileable.id != user.id))
     end
     
-    if not read_only_mode?
-      # Regular users can create, update or destroy own profile fields.
-      # Exceptions:
-      #   They cannot change their membership number.
-      #
-      can [:create, :read, :update, :destroy], ProfileField do |field|
-        field.profileable.nil? || ((field.label != 'W-Nummer') && (field.profileable == user))
-      end
-      
-      # Regular users can update their own validity ranges of memberships
-      # in order to update their corporate vita.
-      #
-      can :update, UserGroupMembership do |user_group_membership|
-        user_group_membership.user == user
-      end
-    end
-    
-    # All users can join events.
+    # # TODO: Wieder aktivieren, falls man die übrigen General-Felder bearbeiten können soll.
+    # # Im Moment können (per your_platform) die Benutzer nur ihre Nicht-General-Felder bearbeiten.
     #
-    can :read, Event
-    if not read_only_mode?
-      can :join, Event
-      can :leave, Event
-    end
-    can :index, Event
-    can :index_events, Group
-    can :index_events, User do |other_user|
-      other_user == user
-    end
-    
-    
-    # Name auto completion
-    #
-    can :autocomplete_title, User
+    # if not read_only_mode?
+    #   # Regular users can create, update or destroy own profile fields.
+    #   # Exceptions:
+    #   #   They cannot change their membership number.
+    #   #
+    #   can [:create, :read, :update, :destroy], ProfileField do |field|
+    #     field.profileable.nil? || ((field.label != 'W-Nummer') && (field.profileable == user))
+    #   end
+    # end
     
     # List exports
     #   - BV-Mitgliedschaft berechtigt dazu, die Mitglieder dieses BV
@@ -229,29 +200,15 @@ module AbilityDefinitions
   # Local officers can export the member lists of their groups.
   #
   def rights_for_local_officers
-    can :export_member_list, Group do |group|
-      user.in? group.officers_of_self_and_ancestor_groups
-    end
+    #
+    # Inherited from your_platform:
+    # - export member list for groups the user is officer of
+    # - sending group mails
+    # - creating events
+    # 
+    super
     
     if not read_only_mode?
-      # Group emails
-      #
-      can :create_post_for, Group do |group|
-        user.in?(group.officers_of_self_and_ancestor_groups) || user.in?(group.corporation.try(:officers) || [])
-      end
-    
-      # Local officers can create events in their groups.
-      #
-      can :create_event, Group do |group|
-        user.in? group.officers_of_self_and_ancestor_groups
-      end
-      can :update, Event do |event|
-        event.group && user.in?(event.group.officers_of_self_and_ancestor_groups)
-      end
-      can :update, Group do |group|
-        group.has_flag?(:contact_people) && can?(:update, group.parent_events.first)
-      end
-      
       # Create, update and destroy Pages
       #
       can :create_page_for, [Group, Page] do |parent|
@@ -293,32 +250,13 @@ module AbilityDefinitions
   
   # ===============================================================================================
   def rights_for_everyone
-    # Imprint
-    # Make sure all users (even if not logged in) can read the imprint.
     #
-    can :read, Page do |page|
-      page.has_flag? :imprint
-    end
-    
-    # Listing Events and iCalendar (ICS) Export:
+    # Inherited from your_platform:
+    # - read imprint
+    # - listing public events
+    # - ics calendar feed
     #
-    # There are event lists on public websites and webcal feeds.
-    # Therefore the user might not be logged in through a regular
-    # session. Some public feeds can be seen by anyone. Other
-    # feeds require an auth token.
-    # 
-    can :index_events, Group do |group|
-      # Any registered user (identified by an auth token)
-      # can index the events of any group.
-      params[:token].present? && UserAccount.find_by_auth_token(params[:token])
-    end
-    can :index_events, User do |other_user|
-      # To index the events relevant to a certain user,
-      # one has to provide the correct auth token that corresponds
-      # to that user.
-      params[:token].present? && (UserAccount.find_by_auth_token(params[:token]) == other_user.account)
-    end
-    can :index_public_events, :all
+    super
     
     # Nobody, not even global admins, can send posts to deceased-groups.
     # Also creating events for those groups is not good.
