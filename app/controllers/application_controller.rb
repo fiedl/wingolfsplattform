76ec476dc253e200735826e7a909a39637ea4aac
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2015-04-22 18:31:44 +0200

    layouts: storing layout setting in cookie, adding compact layout setting.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,16 +11,20 @@ class ApplicationController
   protected
   
   def find_layout
-    
     # TODO: The layout should be saved in the user's preferences, i.e. interface settings.
     layout = "wingolf"
     layout = "bootstrap" if Rails.env.test?
     
-    layout = "minimal" if params[:layout] == "minimal"
-    layout = "wingolf" if params[:layout] == "wingolf"
-    layout = "bootstrap" if params[:layout] == "bootstrap"
-
+    layout = "minimal" if layout_setting == "minimal"
+    layout = "wingolf" if layout_setting == "wingolf"
+    layout = "bootstrap" if layout_setting == "bootstrap"
+    layout = "compact" if layout_setting == "compact"
+    
+    cookies[:layout] = layout
     return layout
+  end
+  def layout_setting
+    params[:layout] || cookies[:layout]
   end
   
 end
