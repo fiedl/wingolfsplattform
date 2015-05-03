f7214eadd0293eb088cab456e791e990a8d5e32c
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2015-05-03 02:09:11 +0200

    Zuständige Administratoren zu admins#index hinzufügen.

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -10,6 +10,9 @@ class AdminsController < ApplicationController
     @global_admins_parent_group = Group.everyone.admins_parent
     @corporations = Corporation.all
     @bvs = Bv.all
+    
+    # responsible are: local admins + last global admin:
+    @admins_responsible_for_me = (current_user.admins_of_self_and_ancestors - Group.global_admins.members[0..-2]).uniq
   end
   
 end
