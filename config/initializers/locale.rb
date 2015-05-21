a4eb4508e021e7ad1c938c3b81bf7e73f4978cd3
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2015-05-21 02:17:32 +0200

    updating your_platform in order to get the notifications feature for beta testers.
    trello: https://trello.com/c/D6ipTrxK/849-benachrichtigungen-notifications

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -4,8 +4,8 @@
 I18n.config.enforce_available_locales = true
 
 # tell the I18n library where to find your translations
-I18n.load_path = Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s] + Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')] + I18n.load_path
- 
+Rails.application.config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s] + Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
+
 # set default locale to something other than :en
 I18n.available_locales = [:de, :en]
 I18n.default_locale = :de
