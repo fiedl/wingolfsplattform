60e83b5bc5fade2e13bfccd786000c767eddfdc7
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2015-05-31 15:23:55 +0200

    updating your_platform to get comments.
    trello:
    * https://trello.com/c/KCWcpdiY/864-kommentare-fur-posts
    * https://trello.com/c/sTcxFe7N/863-korrekturen-an-notifications

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150527151038) do
+ActiveRecord::Schema.define(version: 20150530224106) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id",   limit: 4
@@ -60,6 +60,15 @@ ActiveRecord::Schema.define(version: 20150527151038) do
     t.string   "plz",        limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "text",             limit: 65535
+    t.integer  "author_user_id",   limit: 4
+    t.string   "commentable_type", limit: 255
+    t.integer  "commentable_id",   limit: 4
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
   end
 
   create_table "dag_links", force: :cascade do |t|
