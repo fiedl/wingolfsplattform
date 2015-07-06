34083b66351d595b03c58040c6085df6b615c5bf
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2015-07-06 14:41:40 +0200

    fixing issue where officers#index raised an error.
    'comparison of Fixnum with nil failed'

--- app/controllers/officers_controller.rb
+++ app/controllers/officers_controller.rb
@@ -5,7 +5,7 @@ module OfficersControllerAdditions
   def index
     super
     @chargen = @group.descendant_groups.flagged(:chargen).first.try(:child_groups).try(:sort_by) do |charge|
-      ['senior', 'fuxmajor', 'kneipwart'].index(charge.flags.first.to_s)
+      ['senior', 'fuxmajor', 'kneipwart'].index(charge.flags.first.to_s) || 10
     end || []
     @phil_x = @group.descendant_groups.flagged(:phil_x).first
   end
