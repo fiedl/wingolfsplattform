9791899d5083220f62809e4780336a24587e1735
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2015-09-27 09:47:55 +0200

    Übrige Posts auch blockieren.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,17 @@
 require_dependency YourPlatform::Engine.root.join('app/controllers/posts_controller').to_s
 
 module PostsControllerOverride
+  
+  def create
+    raise "Aufgrund aktueller Missbrauchfälle blockiert."
+  end
 
   private
   
   def create_via_email
     raise "blocked due to current abuse issue."
   end
-
+  
 end  
 
 class PostsController
