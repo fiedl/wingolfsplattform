686f4265123f800a502efe44fa7efb17ef59dc44
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2015-10-21 08:51:11 +0200

    Feature Spec: Lokaler Admin löst einen Status-Workflow aus.
    Trello:
    https://trello.com/c/xRDkzYQN/917-lokale-administratoren-konnen-keine-ak
    tivmeldung-eintragen

--- spec/factories/wingolf_corporation.rb
+++ spec/factories/wingolf_corporation.rb
@@ -20,6 +20,30 @@ FactoryGirl.define do
       philisterschaft.update_attribute(:type, 'Philisterschaft')
       philisterschaft.add_flag :full_members
       Group.alle_philister << philisterschaft
+      
+      status_workflow = Workflow.create name: 'Reception', description: "Macht aus einem Hospitanten einen Kraßfuxen."
+      
+      # # Does not work:
+      # status_workflow.steps.create(brick_name: "RemoveFromGroupBrick", parameters: { :group_id => corporation.status_groups.first.id })
+      # status_workflow.steps.create(brick_name: "AddToGroupBrick", parameters: { :group_id => corporation.status_groups.second.id })
+      
+      step = status_workflow.steps.create
+      step.brick_name = 'RemoveFromGroupBrick'
+      step.save
+      param = step.parameters.create
+      param.key = :group_id
+      param.value = corporation.status_groups.first.id
+      param.save
+
+      step = status_workflow.steps.create
+      step.brick_name = 'AddToGroupBrick'
+      step.save
+      param = step.parameters.create
+      param.key = :group_id
+      param.value = corporation.status_groups.second.id
+      param.save
+      
+      status_workflow.parent_groups << corporation.status_groups.first
     end
   end
 end
