72f60b1acf858598286debb4188ac8ae29974f4a
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2016-04-21 19:08:42 +0200

    BV-Zuordnung: Der Mechanismus soll effizienter arbeiten.
    Anstatt für jedes gespeichertes Sub-Adressfeld eine BV-Neuzuordnung
    auszulösen, nur für das PLZ-Feld; und zwar zehn Sekunden verzögert,
    sodass die anderen Felder auch schon gespeichert sind, wenn die
    Zuordnung ausgeführt wird.
    
    Außerdem reagieren die Adressfelder dann schneller, weil die Zuordnung
    asynchron in einem Background-Job erfolgt, nicht mehr direkt beim
    Speichern.

--- app/models/profile_field.rb
+++ app/models/profile_field.rb
@@ -13,8 +13,8 @@ class ProfileField
   def adopt_profileable_bv
     if self.kind_of?(ProfileFieldTypes::Address) && self.profileable.kind_of?(User) && self.profileable.alive? && self.profileable.wingolfit?
       self.profileable.adapt_bv_to_primary_address
-    elsif self.parent.kind_of?(ProfileFieldTypes::Address)
-      self.parent.adopt_profileable_bv
+    elsif self.parent.kind_of?(ProfileFieldTypes::Address) and self.key == 'postal_code'
+      self.parent.delay_for(10.seconds).adopt_profileable_bv
     end
   end
 
