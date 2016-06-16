f9501fa8102d01b4b2c3e4743a5726006433ceab
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2016-06-16 16:58:26 +0200

    Wingolfsblätter-Adress-Export: Füge zwei Spalten hinzu:
    - ExportUser#postal_address
    - ExportUser#postal_address_postal_code_and_town
    
    Trello: https://trello.com/c/8aJrWWpE/1002

--- app/models/list_exports/wingolfsblaetter.rb
+++ app/models/list_exports/wingolfsblaetter.rb
@@ -1,6 +1,6 @@
 module ListExports
   class Wingolfsblaetter < Base
-    
+
     def columns
       [
         :last_name,
@@ -11,18 +11,20 @@ module ListExports
         :postal_address_second_address_line,
         :postal_address_postal_code,
         :postal_address_town,
-        :postal_address_country
+        :postal_address_country,
+        :postal_address,
+        :postal_address_postal_code_and_town
       ]
     end
-    
+
     def data_rows
       super.select do |user|
         user.wingolfit? &&
         user.alive? &&
-        user.postal_address_field_or_first_address_field && 
+        user.postal_address_field_or_first_address_field &&
         user.postal_address_field_or_first_address_field.issues.none?
       end.uniq
     end
-    
+
   end
 end
