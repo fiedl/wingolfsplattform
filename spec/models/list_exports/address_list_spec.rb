b7e6770aa908b2e974094456d76b9122b941603e
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2016-07-27 00:53:03 +0200

    Adress-List-Export: Spalte für Funktion in der exportierten Gruppe.
    Beispiel: „Philister-x, Freiburger Wingolf“. Dadurch kann man eine
    Amtsträger-Sammel-Gruppe exportieren und wissen, welcher Amtsträger zu
    welchem Verein gehört.
    
    YourPlatform:
    https://github.com/fiedl/your_platform/commit/26373598b8e0f53d40f05b821a
    cf0de11ab98f12
    
    Trello: https://trello.com/c/EZb4qYrZ/1010-phil-x-liste
    
    Ticket: http://support.wingolfsplattform.org/tickets/970
    
    Siehe auch: https://trello.com/c/r427f7K7/903-listexport

---
+++ spec/models/list_exports/address_list_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe ListExports::AddressList do
+  before do
+    @group = create :group, name: "Gruppe"
+    @corporation = create :corporation_with_status_groups
+    @bv = create :bv_group, name: 'BV 01', token: 'BV 01'
+    @user = create :user
+
+    @group.assign_user @user
+    @corporation.status_groups.first.assign_user @user  # in order to give the @user a #title other than his #name.
+    @bv.assign_user @user
+
+    @user_title_without_name = @user.title.gsub(@user.name, '').strip
+
+    @address1 = @user.profile_fields.create(type: 'ProfileFieldTypes::Address', value: "Pariser Platz 1\n 10117 Berlin")
+    @address1.update_column(:updated_at, "2014-06-20".to_datetime)
+    @name_surrounding = @user.profile_fields.create(type: 'ProfileFieldTypes::NameSurrounding').becomes(ProfileFieldTypes::NameSurrounding)
+    @name_surrounding.name_prefix = "Dr."
+    @name_surrounding.name_suffix = "M.Sc."
+    @name_surrounding.text_above_name = "Herrn"
+    @name_surrounding.text_below_name = ""
+    @name_surrounding.save
+    @user.save
+
+    @list_export = ListExports::AddressList.from_group(@group)
+  end
+
+  describe "#headers" do
+    subject { @list_export.headers }
+    it { should include "BV"}
+  end
+
+  describe "#to_csv" do
+    subject { @list_export.to_csv }
+    it { should include "BV 01"}
+  end
+end
