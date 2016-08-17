82e49d34aa962c0f9c3f89d6a70771455c209f88
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2016-08-17 16:11:39 +0200

    Pages::WohnenImWingolf aus sf/wingolf-org importieren, damit die lokale Entwicklungsdatenbank nicht durch das STI verwirrt wird.

---
+++ app/models/pages/wohnen_im_wingolf.rb
@@ -0,0 +1,18 @@
+# Dies ist die "Wohnen"-Seite der öffentlichen Homepage wingolf.org.
+# Diese Seite ist mit allen Wingolfshäusern bebildert. Um dies zu automatisieren,
+# überschreibt diese Sub-Klasse die Attachments-Methode.
+#
+class Pages::WohnenImWingolf < Page
+
+  # To save us from managing separate routes and controllers for this
+  # subclass, override the model name.
+  #
+  def self.model_name
+    Page.model_name
+  end
+
+  def to_partial_path
+    'pages/page'
+  end
+
+end
