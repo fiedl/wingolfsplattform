c3b2bb886bc68c7d4243b06474c2faa14ddc2027
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2016-09-28 17:55:22 +0200

    Semesterprogramme: Als Ansprechpartner den Kneipwart eintragen,
    nicht den aktuellen Benutzer. Der Ansprechpartner kann für einzelne Veranstaltungen in der Detail-Ansicht events#show geändert werden.
    
    Trello: https://trello.com/c/ve77mrU4/1028-semesterprogramm-bearbeiten-2016-iso1aigh

--- app/helpers/semester_calendars_helper.rb
+++ app/helpers/semester_calendars_helper.rb
@@ -6,4 +6,8 @@ module SemesterCalendarsHelper
     [:aktive, :philister, :publish_on_local_website, :publish_on_global_website]
   end
 
+  def semester_calendar_default_contact_person(semester_calendar)
+    semester_calendar.kneipwart
+  end
+
 end
