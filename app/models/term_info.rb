680b2064e8489712b4c4d8a6e31f8f8ba8eda517
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2017-01-15 00:11:56 +0100

    updating your_platform to get term info model
    https://trello.com/c/0o1V9xaf/1092-automatische-semester-statistik

---
+++ app/models/term_info.rb
@@ -0,0 +1,15 @@
+require_dependency YourPlatform::Engine.root.join('app/models/term_info').to_s
+
+class TermInfo
+  #Anzahl Aktivmeldungen
+  #Anzahl Philistrationen
+  #Anzahl Austritte
+  #Anzahl Todesfälle
+  #
+  #Anzahl Aktive
+  #Anzahl Philister
+  #Anzahl Amtsträger
+  #
+  #Anzahl Veranstaltungen
+  ## Anzahl Kalender-Abonnenten
+end
