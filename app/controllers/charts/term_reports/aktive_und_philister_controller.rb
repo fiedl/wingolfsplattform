158d8202169f845e5eb9bc23e5441e1f50a8a076
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2017-01-19 15:25:58 +0100

    term_reports#show: Die Daten des Charts erstellen, wenn sie nicht existieren.
    Dabei den Controller auf *decent_exposure* umstellen.
    
    https://trello.com/c/0o1V9xaf/1092-term-infos-2017-cee9ievu

--- app/controllers/charts/term_reports/aktive_und_philister_controller.rb
+++ app/controllers/charts/term_reports/aktive_und_philister_controller.rb
@@ -1,29 +1,31 @@
 class Charts::TermReports::AktiveUndPhilisterController < ChartsController
 
+  # https://railscasts.com/episodes/259-decent-exposure
+  #
+  expose :corporation
+  expose :terms, -> {
+    # Wir zeigen hier willkürlich die letzten zehn Jahre an.
+    Terms::Year.where(year: (Time.zone.now.year-10)..Time.zone.now.year)
+  }
+  expose :term_reports, -> {
+    terms.collect { |term| ::TermReports::ForCorporation.by_corporation_and_term(corporation, term) }
+  }
+
   # GET /charts/term_reports/aktive_und_philister/anzahl_per_jahr.json?corporation_id=12
   #
   def anzahl_per_jahr
     authorize! :index, :charts
 
-    # Die letzten zehn Jahre
-    term_ids = Terms::Year.where(year: (Time.zone.now.year-10)..Time.zone.now.year).pluck(:id)
-
     render json: [
       {
         name: "Philister",
-        data: Hash[corporation.term_reports.where(term_id: term_ids).collect { |term_report| [term_report.term.title, term_report.anzahl_philister] }]
+        data: Hash[term_reports.collect { |term_report| [term_report.term.title, term_report.anzahl_philister] }]
       },
       {
         name: "Aktive",
-        data: Hash[corporation.term_reports.where(term_id: term_ids).collect { |term_report| [term_report.term.title, term_report.anzahl_aller_aktiven] }]
+        data: Hash[term_reports.collect { |term_report| [term_report.term.title, term_report.anzahl_aller_aktiven] }]
       }
     ]
-  end
-
-  private
-
-  def corporation
-    Corporation.find params[:corporation_id]
   end
 
 end
