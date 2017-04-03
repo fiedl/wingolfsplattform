56359273166a0d6e5eff384148667dac04dcbfb3
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2017-04-03 23:07:57 +0200

    updating your_platform to get [master 1a877db] trying to fix mailer from field.

--- app/mailers/user_account_mailer.rb
+++ app/mailers/user_account_mailer.rb
@@ -1,9 +1,10 @@
 class UserAccountMailer < ActionMailer::Base
-  def welcome_email( user, password ) 
+  def welcome_email(user, password)
     @user = user
     @password = password
     @bundesbruder_or_philister = @user.philister? ? "Philister" : "Bundesbruder"
-    
+    @from = Setting.support_email
+
     # Bundesbrüder, die nur in Estland aktiv sind, bekommen diese E-Mail auf englisch,
     # alle anderen auf deutsch.
     #
@@ -12,13 +13,13 @@ class UserAccountMailer < ActionMailer::Base
     else
       locale = :de
     end
-    
-    
+
+
     to = "#{@user.name} <#{@user.email}>"
-    
+
     I18n.with_locale(locale) do
-      mail to: to, subject: t( :welcome_to_wingolfsplattform )
+      mail to: to, subject: t(:welcome_to_wingolfsplattform), from: @from
     end
-    
+
   end
 end
