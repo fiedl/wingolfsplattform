40a86c8ba3dcbdb12ed430eb0f54f97909718791
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2017-04-14 21:34:45 +0200

    Implementiere `Groups::PhvsParent`.
    Diese Überblicks-Gruppe wird alle Philistervereine (PhV) enthalten und
    deren Amtsträger, Mitgliederzahl sowie Anzahl der Erstbandträger
    (Erstbandphilister) auflisten.
    
    https://trello.com/c/5pZr1QEl/1119-liste-der-phv-mit-erstbandtragern

---
+++ app/models/groups/phvs_parent.rb
@@ -0,0 +1,15 @@
+class Groups::PhvsParent < Groups::GroupOfGroups
+
+  def important_officer_keys
+    [:phil_x, :schriftwart, :kassenwart]
+  end
+
+  def child_groups_table_rows
+    rows = super
+    rows.each do |row|
+      philisterverein = Group.find row[:child_group_id]
+      row[:erstbandtraeger_count] = philisterverein.erstbandphilister.count
+    end
+    rows
+  end
+end
