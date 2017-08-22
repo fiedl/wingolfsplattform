57968823da4d866af4e2b4823c11cd2d5b880e82
Sebastian Fiedlschuster <github@fiedlschuster.de>
2017-08-22 21:39:41 +0200

    `config.assets.digest = true` wird in der Procudtion-Konfiguration nicht benötigt, weil es der Standard-Einstellung von Rails entspricht.
    https://trello.com/c/oIddSLU0/1161-rails-5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,8 +22,6 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  config.assets.digest = true
-
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
