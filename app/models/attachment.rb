aa14e1d1527273c1e6f2769adf97392d63c96911
Sebastian Fiedlschuster <github@fiedlschuster.de>
2017-09-10 15:23:06 +0200

    Wohnen im Wingolf: Berücksichtige nur Anhänge von Gruppen, nicht von Seiten,
    da sonst auch Inhalte aus internen Blogs einbezogen werden.
    
    https://trello.com/c/JZzRpifA/1201-single-page-layout-anpassungen-für-wi
    ngolforg-2017-xood7uid

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,6 +1,6 @@
 require_dependency YourPlatform::Engine.root.join('app/models/attachment').to_s
 
 class Attachment
-  scope :wingolfshaus, -> { where("title LIKE ?", "%Wingolfshaus%") }
+  scope :wingolfshaus, -> { where("title LIKE ?", "%Wingolfshaus%").where(parent_type: 'Group') }
   scope :wappen, -> { where("title LIKE ?", "%Wappen%") }
 end
