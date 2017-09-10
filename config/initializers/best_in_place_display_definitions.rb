0aee81e04edc350bc3e37787762d641474e6cbe2
Sebastian Fiedlschuster <github@fiedlschuster.de>
2017-09-10 15:20:49 +0200

    Füge Best-in-place-Definitionen hinzu, um den Seitentyp "Wohnen im Wingolf" inline bearbeiten zu können.
    https://trello.com/c/JZzRpifA/1201-single-page-layout-anpassungen-für-wi
    ngolforg-2017-xood7uid

---
+++ config/initializers/best_in_place_display_definitions.rb
@@ -0,0 +1,2 @@
+BestInPlace::DisplayMethods.add_helper_method(Pages::WohnenImWingolf, :content, :page_markup)
+BestInPlace::DisplayMethods.add_helper_method(Pages::WohnenImWingolf, :teaser_text, :page_markup)
