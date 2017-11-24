16309f815fa247a161d038105bfe9f4d64da8371
Sebastian Fiedlschuster <github@fiedlschuster.de>
2017-11-24 09:43:52 +0100

    Repariere Impressum-Spec.
    Die Impressum-Seite ist nun nicht mehr automatisch im Footer, sondern
    wird durch den `:footer`-Flag hineingeholt.
    
    https://trello.com/c/UVpa6ee7/1216-inbetriebnahme-des-homepage-moduls-fü
    r-wingolforg-2017-zu8ahm2i

--- spec/features/impressum_spec.rb
+++ spec/features/impressum_spec.rb
@@ -5,8 +5,8 @@ feature 'Impressum' do
 
   describe 'for an imprint Page existing' do
     background do
-      @imprint = Page.find_root.child_pages.create(title: "Imprint", content: "This is the imprint.", published_at: 1.day.ago)
-      @imprint.add_flags :imprint
+      @imprint = Page.find_root.child_pages.create(title: t(:imprint), content: "This is the imprint.", published_at: 1.day.ago)
+      @imprint.add_flags :imprint, :footer
     end
     scenario 'clicking on the imprint link in the footer' do
       login(:user)
@@ -24,18 +24,6 @@ feature 'Impressum' do
         click_on I18n.t(:imprint)
       end
       page.should have_content "This is the imprint."
-    end
-  end
-
-  describe 'for no imprint Page existing' do
-    scenario 'clicking on the imprint link in the footer' do
-      login(:user)
-      visit root_path
-
-      within "#footer" do
-        expect { click_on I18n.t(:imprint) }.not_to raise_error
-      end
-
     end
   end
 end
