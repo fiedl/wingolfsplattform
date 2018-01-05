e318a2295090e8b776eb1b18d8f06d3a9b14a52b
Sebastian Fiedlschuster <github@fiedlschuster.de>
2018-01-05 23:46:27 +0100

    Renew-Cache wieder aktivieren.
    Die beiden Methoden, um zum vorigen Mechanismus zurückzufallen, haben nicht problemlos funktioniert. Die erste Variante war noch besser, erneuterte aber nicht alle Methoden, sodass ein Spec fehlschlug. Die zweite lässt einige Specs fehlschlagen, vermutlich, weil durch den Override die Verkettung von `fill_cache` nicht mehr funktioniert.
    
    Ich aktiviere den neuen Mechanismus wieder, damit zumindest der aktuelle Patch deployt werden kann. Danach muss der Rückfall in den alten Caching-Mechanismus nocheinmal systematisch angegangen werden.
    
    https://trello.com/c/QyxiGXkw/1228-export-änderungen-für-willi
    https://trello.com/c/TIa4DEpK/1115-erneuerung-des-caching-systems-2016-u

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,10 +2,4 @@ require_dependency YourPlatform::Engine.root.join('app/models/application_record
 
 class ApplicationRecord
 
-  # Deaktiviere den experimentellen Renew-Cache-Mechanismus für den Moment.
-  # https://trello.com/c/QyxiGXkw/1228
-  def renew_cache(options = {})
-    delete_cache
-  end
-
 end
