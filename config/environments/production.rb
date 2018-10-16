6e091644290454414f52cdf31934d63afb97eb64
Sebastian Fiedlschuster <github@fiedlschuster.de>
2018-10-17 00:16:39 +0200

    trying to fix uglifier issue
    Uglifier::Error: Unexpected token: punc ({). To use ES6 syntax, harmony mode must be enabled with Uglifier.new(:harmony => true).

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,8 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # https://github.com/lautis/uglifier/issues/127#issuecomment-352224986
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
