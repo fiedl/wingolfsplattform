5a51d3b07e1da6451534181b1fe9b0974f22e156
Sebastian Fiedlschuster <github@fiedlschuster.de>
2019-08-08 16:38:32 +0200

    Passe Aktivmeldungs-Mailer an neue Spam-Richtlinien an.
    https://trello.com/c/s94OXzul/1371-e-mails-554-570-reject

--- app/mailers/user_account_mailer.rb
+++ app/mailers/user_account_mailer.rb
@@ -1,9 +1,8 @@
-class UserAccountMailer < ActionMailer::Base
+class UserAccountMailer < BaseMailer
   def welcome_email(user, password)
     @user = user
     @password = password
     @bundesbruder_or_philister = @user.philister? ? "Philister" : "Bundesbruder"
-    @from = Setting.support_email
 
     # Bundesbrüder, die nur in Estland aktiv sind, bekommen diese E-Mail auf englisch,
     # alle anderen auf deutsch.
@@ -15,10 +14,10 @@ class UserAccountMailer < ActionMailer::Base
     end
 
 
-    to = "#{@user.name} <#{@user.email}>"
+    @to = "\"#{@user.title}\" <#{@user.email}>"
 
     I18n.with_locale(locale) do
-      mail to: to, subject: t(:welcome_to_wingolfsplattform), from: @from
+      mail to: @to, subject: t(:welcome_to_wingolfsplattform)
     end
 
   end
