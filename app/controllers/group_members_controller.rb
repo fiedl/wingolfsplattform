0e712219e5211e114dcd48dccb3a12eb06a45946
Sebastian Fiedlschuster <github@fiedlschuster.de>
2019-10-21 00:03:33 +0200

    adding missing import of your-platform's `GroupMembersController`
    https://trello.com/c/MzmipIol/1448-aktivmeldungsbutton-zurückholen

--- app/controllers/group_members_controller.rb
+++ app/controllers/group_members_controller.rb
@@ -1,6 +1,8 @@
+require_dependency YourPlatform::Engine.root.join('app/controllers/group_members_controller').to_s
+
 class GroupMembersController
   def new
-    authorize! :create, User 
+    authorize! :create, User
     redirect_to controller: 'aktivmeldungen', action: 'new'
   end
 end
