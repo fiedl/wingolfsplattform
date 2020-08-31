065291cf9f3915793ed8245b4d520cad84b72403
Sebastian Fiedlschuster <github@fiedlschuster.de>
2020-08-31 16:30:26 +0200

    Layout-Korrekturen, YouTube-Links, Semesterstatistiken
    Updating YourPlatform Engine
    
    - [9e120ab](https://github.com/fiedl/your_platform/commit/9e120ab) search results: fix search-hint styling
    - [cfe592b](https://github.com/fiedl/your_platform/commit/cfe592b) accommodations#index: adding missing profile-card body
    - [b3f50ba](https://github.com/fiedl/your_platform/commit/b3f50ba) groups#index: adding button to send messages
    - [7b05535](https://github.com/fiedl/your_platform/commit/7b05535) public pages: set font size absolute to avoid overlarge uls
    - [17cd912](https://github.com/fiedl/your_platform/commit/17cd912) posts: fix styling
    - [9bb1976](https://github.com/fiedl/your_platform/commit/9bb1976) term_reports#index
    - [334ca36](https://github.com/fiedl/your_platform/commit/334ca36) term reports: remove "year" and headers from select
    - [fb9280a](https://github.com/fiedl/your_platform/commit/fb9280a) users#show: fix button styling
    - [5889fc1](https://github.com/fiedl/your_platform/commit/5889fc1) posts form: fixing styling issues
    - [59fa91f](https://github.com/fiedl/your_platform/commit/59fa91f) make post attachments dependent
    - [c513d17](https://github.com/fiedl/your_platform/commit/c513d17) navbar: posts
    - [0a603b0](https://github.com/fiedl/your_platform/commit/0a603b0) [wip] pasting youtube links to wysiwyg
    
    https://trello.com/c/5T2kptV3/1475-tabler-layout

--- app/controllers/groups/wohnheimsvereine_controller.rb
+++ app/controllers/groups/wohnheimsvereine_controller.rb
@@ -9,5 +9,17 @@ class Groups::WohnheimsvereineController < ApplicationController
     set_current_title group.title
   end
 
+  def update
+    authorize! :update, group
+
+    group.update! group_params
+    render json: {}, status: :ok
+  end
+
+  private
+
+  def group_params
+    params.require(:groups_wohnheimsverein).permit(:name, :body, :avatar, :avatar_background)
+  end
 
 end
